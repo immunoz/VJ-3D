28efcc15180ac24aef2120d050015e6f80f6c954
immunoz <isaac-mc99@hotmail.com>
2021-05-07 01:30:01 +0200

    cooking time fixed

--- VJ-Overcooked/Assets/Scripts/Pot.cs
+++ VJ-Overcooked/Assets/Scripts/Pot.cs
@@ -9,11 +9,25 @@ public class Pot : MonoBehaviour
 
     public int count;
     public string ingredient;
+    private float cookTime;
+    public float MaxCookTime;
 
     void Start()
     {
         count = 0;
         ingredient = "";
+        cookTime = 0;
+    }
+
+    private void Update()
+    {
+        //Debug.Log("ksdhafklashkflj");
+        Debug.Log(MaxCookTime * count / 3);
+        if (count != 0 && cookTime < MaxCookTime * count / 3) {
+            cookTime += Time.deltaTime;
+            //GetComponent<ProcessBar>().setProcessTime(cookTime/MaxCookTime); //cookTime - (cookTime / (MaxCookTime * count))
+            GetComponent<ProcessBar>().setProcessTime(cookTime/ (MaxCookTime * count / 3)); //cookTime - (cookTime / (MaxCookTime * count))
+        }
     }
 
     public bool setObject(GameObject obj)
@@ -21,6 +35,10 @@ public class Pot : MonoBehaviour
         Ingredient ingredientScript = obj.GetComponent<Ingredient>();
         if (ingredientScript.inPot() && ingredientScript.choppingDone() && (count == 0 || obj.name == ingredient && count < 3) ) {
             count++;
+            //if (count > 1) cookTime -= MaxCookTime / count;
+            //cookTime = cookTime - (cookTime / (MaxCookTime * count-1));
+            //cookTime /= MaxCookTime;
+            GetComponent<ProcessBar>().setMaxTime(1);
             ingredient = obj.name;
             Destroy(obj);
             return true;
